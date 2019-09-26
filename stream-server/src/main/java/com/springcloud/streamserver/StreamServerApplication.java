fc86f2528ff8b4552037832877bd40ae7979eb64
XTzhangfei <1003267232@qq.com>
2019-09-26 11:37:35 +0800

    后端

---
+++ stream-server/src/main/java/com/springcloud/streamserver/StreamServerApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.streamserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamServerApplication.class, args);
+	}
+
+}
